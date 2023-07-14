4ac7f7c40d201e55281505776d678bc34b21bd5b
Gowdilya Jeyakumar <gowdilyajeyakumar@Gowdilyas-MacBook-Pro-2.local>
2023-07-14 15:06:20 -0400

    Update

---
+++ src/components/Resume/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Resume.css";
+import { resumeHTML } from "./html";
+//var __html = require("./html.js");
+//var template = { __html: __html };
+
+export default function Resume() {
+  return (
+    <div>
+      <div className="screen-share">
+        <span dangerouslySetInnerHTML={resumeHTML} />
+      </div>
+    </div>
+  );
+}

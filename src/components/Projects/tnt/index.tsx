9b663b97fb02cef40e8ab6019b700392c2c8c5a3
Gowdilya Jeyakumar <gowdilyajeyakumar@Gowdilyas-MacBook-Pro-2.local>
2023-08-13 22:20:23 -0400

    update

--- src/components/Projects/tnt/index.tsx
+++ src/components/Projects/tnt/index.tsx
@@ -11,16 +11,7 @@ import "./tnt.scss";
 // import image7 from "/images/7.jpg";
 // import image8 from "/images/8.jpg";
 // import image9 from "/images/9.jpg";
-//ghpages
-import image1 from "images/1.jpg";
-import image2 from "images/2.jpg";
-import image3 from "images/3.jpg";
-import image4 from "images/4.jpg";
-import image5 from "images/5.jpg";
-import image7 from "images/7.jpg";
-import image8 from "images/8.jpg";
-import image9 from "images/9.jpg";
-import image10 from "images/10.jpg";
+
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
@@ -29,31 +20,31 @@ export default function Tnt() {
     <div>
       <Carousel>
         <div>
-          <img src={image1} className="sliderimg" alt="" />
+          <img src={"images/1.jpg"} className="sliderimg" alt="" />
         </div>
         <div>
-          <img src={image2} className="sliderimg" alt="" />
+          <img src={"images/2.jpg"} className="sliderimg" alt="" />
         </div>
         <div>
-          <img src={image3} className="sliderimg" alt="" />
+          <img src={"images/3.jpg"} className="sliderimg" alt="" />
         </div>
         <div>
-          <img src={image4} className="sliderimg" alt="" />
+          <img src={"images/4.jpg"} className="sliderimg" alt="" />
         </div>
         <div>
-          <img src={image5} className="sliderimg" alt="" />
+          <img src={"images/5.jpg"} className="sliderimg" alt="" />
         </div>
         <div>
-          <img src={image7} className="sliderimg" alt="" />
+          <img src={"images/7.jpg"} className="sliderimg" alt="" />
         </div>
         <div>
-          <img src={image8} className="sliderimg" alt="" />
+          <img src={"images/8.jpg"} className="sliderimg" alt="" />
         </div>
         <div>
-          <img src={image9} className="sliderimg" alt="" />
+          <img src={"images/9.jpg"} className="sliderimg" alt="" />
         </div>
         <div>
-          <img src={image10} className="sliderimg" alt="" />
+          <img src={"images/10.jpg"} className="sliderimg" alt="" />
         </div>
       </Carousel>
     </div>
